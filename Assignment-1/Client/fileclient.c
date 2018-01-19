eadc6c543cbc3d0ab70e32c1dee858f16d210614
Projit_B <projit1234@hotmail.com>
2018-01-19 20:07:40 +0530

    sending multiple files

--- Assignment-1/Client/fileclient.c
+++ Assignment-1/Client/fileclient.c
@@ -10,7 +10,7 @@
 char buffer[1024] = {0};
 char filename[1024] = {0};
 char response[100] = {0};
-
+char continueSend[10] = {0};
 
 int main(int argc, char const *argv[])
 {
@@ -50,44 +50,64 @@ int main(int argc, char const *argv[])
   // Below is the Interaction that we need to worry about. Above is all boilerplate.
 
   /*File Transfer Below*/
-    
-  //Response with file name
-  printf("Enter file name: ");
-  scanf("%s", filename);
-  send(sock, filename, strlen(filename), 0);
-  
-  
-  //This is where we hopefully receive Sending... from the server
-  for(i = 0; i < 1024; response[i++] = '\0');
-  valread = read(sock, buffer, 1024);
-  for(i = 0; i < valread; response[i] = buffer[i], i++);
-  response[valread] = '\0';
+  while(1){  
 
-  //If Sending, then prepare to write into the file
-  if (strcmp(response, "Sending...") == 0)
-    {
-      FILE* fp = fopen(filename, "wb"); //Create the file
-      send(sock, "Ok", strlen("Ok"), 0); //Response cycle, to help with writing.
-      while(stillRead)
-        {
-          valread = read(sock, buffer, 1024);
-          // Exiting the Writing Loop when Server says Done.
-          if(valread == 4){
-            buffer[4] = '\0';
-            if(strcmp(buffer, "Done") == 0){
-              stillRead = 0;
-              printf("Done\n");
-              break;
+    //Response with file name
+    printf("Enter file name: ");
+    fflush(stdout);
+    scanf("%s", filename);
+    send(sock, filename, strlen(filename), 0);
+  
+  
+    //This is where we hopefully receive Sending... from the server
+    for(i = 0; i < 1024; response[i++] = '\0');
+    valread = read(sock, buffer, 1024);
+    for(i = 0; i < valread; response[i] = buffer[i], i++);
+    response[valread] = '\0';
+
+    //If Sending, then prepare to write into the file
+    if (strcmp(response, "Sending...") == 0)
+      {
+        FILE* fp = fopen(filename, "wb"); //Create the file
+        send(sock, "Ok", strlen("Ok"), 0); //Response cycle, to help with writing.
+        while(stillRead)
+          {
+            valread = read(sock, buffer, 1024);
+            // Exiting the Writing Loop when Server says Done.
+            if(valread == 4){
+              buffer[4] = '\0';
+              if(strcmp(buffer, "Done") == 0){
+                stillRead = 0;
+                printf("Done\n");
+                break;
+              }
             }
+            fseek(fp, 0, SEEK_END);
+            fwrite(buffer, valread, 1, fp);
+            send(sock, done, strlen(done), 0);
           }
-          fseek(fp, 0, SEEK_END);
-          fwrite(buffer, valread, 1, fp);
-          send(sock, done, strlen(done), 0);
-        }
+
+      }
+    else {
+      printf("%s\n", response);
     }
-  else {
-    printf("%s\n", response);
+    printf("Ask for another file?(Yes/No): ");
+    fflush(stdout);
+    scanf("%s", continueSend);
+    if (strcmp(continueSend, "Yes") == 0 || strcmp(continueSend, "yes") == 0 || strcmp(continueSend, "Y") == 0 || strcmp(continueSend, "y") == 0) {
+      send(sock, "Sending More", strlen("Sending More"), 0);
+      valread = read(sock, buffer, 1024);
+      for(i = 0; i < valread; printf("%c", buffer[i++]));
+      printf("\n");
+      stillRead = 1;
+    }
+    else{
+      send(sock, "No More", strlen("No More"), 0);
+      valread = read(sock, buffer, 1024);
+      break;
+    }
   }
+    
   printf("Closing Connection\n");
   fflush(stdout);
   return 0;
